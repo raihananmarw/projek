3896372948ca0b67b7b94317c5f3528819fab825
raihananmarw <raihananmarw@gmail.com>
2025-04-08 13:14:19 +0700

    Delete assets directory

--- assets/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Asset
-
-class AssetForm(forms.ModelForm):
-    class Meta:
-        model = Asset
-        fields = ['nama', 'kategori', 'tanggal_pembelian', 'status']
